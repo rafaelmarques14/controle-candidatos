d106e189d9d3c22245d9e2b2bffa82f793f2eaee
rafaelmarques14 <rafaelmarques14@hotmail.com>
2024-06-27 14:24:05 -0300

    add classe

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -2,7 +2,6 @@ package candidatura;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-
         analisarCandidato(1900.0);
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);
